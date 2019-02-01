0c8f98b5c8650c5888964cba69c5dbf1f59e8e85
Harry_Coburn <harryjcoburn@gmail.com>
2019-02-01 18:11:54 -0500

    Preparing for unit testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,15 @@
+// webpack.config.js
 module.exports = {
-  entry: ["./src/index.js"],
-  devtool: "inline-source-map",
-  output: {
-    path: __dirname + "/dist",
-    filename: "[name].js"
-  },
-  watch: true,
-  devServer: {
-    contentBase: "./src",
-    compress: true,
-    port: 9000
-  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
       }
     ]
